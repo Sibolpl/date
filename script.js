cb82dce645703554f812cfec9891cabea67c3a94
Sibolpl <sibolpl@interia.pl>
2026-06-02 17:22:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,41 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.top =
         Math.random() * (window.innerHeight - 50) + "px";
 });
+
+async function sendForm() {
+
+    const payload = {
+        date: document.getElementById("date").value,
+        time: document.getElementById("time").value,
+        message: document.getElementById("message").value
+    };
+
+    try {
+
+        const response = await fetch(
+            "https://date-webhook.sibolpl.workers.dev/",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(payload)
+            }
+        );
+
+        if (response.ok) {
+            alert("Wysłano ❤️");
+
+            document.getElementById("step2").innerHTML = `
+                <h1>Dziękuję ❤️</h1>
+                <p>Termin został zapisany.</p>
+            `;
+        } else {
+            alert("Błąd wysyłania.");
+        }
+
+    } catch (error) {
+        console.error(error);
+        alert("Nie udało się połączyć z serwerem.");
+    }
+}
