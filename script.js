597b7691d17b0727c9f7381c608926232f1ebe1b
Sibolpl <sibolpl@interia.pl>
2026-06-02 17:33:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const yesBtn = document.getElementById("yes");
 const noBtn = document.getElementById("no");
+const sendBtn = document.getElementById("send");
 
 yesBtn.addEventListener("click", () => {
     document.getElementById("step1").style.display = "none";
@@ -8,46 +9,39 @@ yesBtn.addEventListener("click", () => {
 
 noBtn.addEventListener("mouseover", () => {
     noBtn.style.position = "absolute";
-
-    noBtn.style.left =
-        Math.random() * (window.innerWidth - 100) + "px";
-
-    noBtn.style.top =
-        Math.random() * (window.innerHeight - 50) + "px";
+    noBtn.style.left = Math.random() * (window.innerWidth - 100) + "px";
+    noBtn.style.top = Math.random() * (window.innerHeight - 50) + "px";
 });
 
-async function sendForm() {
+sendBtn.addEventListener("click", sendForm);
 
+async function sendForm() {
     const payload = {
         date: document.getElementById("date").value,
         time: document.getElementById("time").value,
+        place: document.getElementById("place").value,
+        outfit: document.getElementById("outfit").value,
+        food: document.getElementById("food").value,
         message: document.getElementById("message").value
     };
 
     try {
-
-        const response = await fetch(
-            "https://date-webhook.sibolpl.workers.dev/",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(payload)
-            }
-        );
+        const response = await fetch("https://date-webhook.sibolpl.workers.dev/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
 
         if (response.ok) {
-            alert("Wysłano ❤️");
-
             document.getElementById("step2").innerHTML = `
                 <h1>Dziękuję ❤️</h1>
-                <p>Termin został zapisany.</p>
+                <p>Termin został wysłany.</p>
             `;
         } else {
             alert("Błąd wysyłania.");
         }
-
     } catch (error) {
         console.error(error);
         alert("Nie udało się połączyć z serwerem.");
